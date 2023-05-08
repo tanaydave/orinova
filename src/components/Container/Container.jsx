4f4eef5984df81ef22fda7f8b7d9634607ce37a0
Tanay <daveth1106@gmail.com>
2023-05-08 08:50:03 +0530

    deleted unwanted files

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import {RxMagnifyingGlass} from 'react-icons/rx'
-
+import SearchIcon from '@mui/icons-material/Search';
 const Container = () => {
   return (
     <div className="flex flex-col justify-center items-center pt-48">
       <div className="pt-12 pb-12 text-8xl ">Google</div>
       
-      <input type='text' className="w-2/6 pt-2 pb-2 pl-12 rounded-3xl" placeholder=" Search Google or type a URL "/>
+      <input type='text' className="w-2/6 pt-2 pb-2 pl-10 rounded-3xl" placeholder="&#x1F50D;   Search Google or type a URL   &#xe029; "/>
+    <div className=' pt-4  grid grid-cols-5 w-80 items-center'>
+        <div>hi</div>
+        <div>hey</div>
+        <div>hello</div>
+        <div>hello</div>
+
+        <div>hello</div>
+
+
+
     </div>
+    
+    </div>
+
   )
 }
 
